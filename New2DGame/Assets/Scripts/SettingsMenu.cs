84b178130b422dcd7e6fbb86a27e6f5dc9329467
xrawenx <xxnemisisxx@outlook.com>
2023-02-09 18:09:43 +0100

    Fixing resolution options

--- New2DGame/Assets/Scripts/SettingsMenu.cs
+++ New2DGame/Assets/Scripts/SettingsMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class SettingsMenu : MonoBehaviour
 
     public Dropdown resolutionDropdown;
 
-    Resolution[] resolutions;
+    private Resolution[] resolutions;
 
     void Start ()
     {
@@ -18,15 +19,30 @@ public class SettingsMenu : MonoBehaviour
 
         resolutionDropdown.ClearOptions();
 
-        List<string> options = new List<string> ();
+        List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
-            options.Add (option);
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetVolume (float volume)
