d5076766d6b47746dd7e76890f628d8c953049a9
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-02-22 13:10:45 +0100

    Activate Enemy Trigger

---
+++ New2DGame/Assets/Scripts/Simple Scripts/TriggerEeventActivateEnemy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEeventActivateEnemy : MonoBehaviour
+{
+    public Enemy enemy;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("EventEnemyActivate");
+            enemy.gameObject.GetComponentInChildren<Enemy>().enabled = true;
+        }        
+    }
+}
