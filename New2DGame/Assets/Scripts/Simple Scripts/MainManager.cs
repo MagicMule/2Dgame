0157ea2711f991e4624e33a839aca842e065f215
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-03-08 14:24:30 +0100

    Trying to save score

---
+++ New2DGame/Assets/Scripts/Simple Scripts/MainManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainManager : MonoBehaviour
+{
+    // Trying to save score
+    public static MainManager instance;
+
+    public int score;
+
+    public TextMeshProUGUI highScoreText;
+    private void Update()
+    {
+        UpdateHighScore();
+    }
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this; 
+        DontDestroyOnLoad(gameObject);
+    }
+    public void UpdateHighScore()
+    {
+        highScoreText.text = "High Score: " + score;
+    }
+}
