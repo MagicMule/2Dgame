cf08f3f7668738a2d2dfbbaed431b8bae6e2dce3
xrawenx <xxnemisisxx@outlook.com>
2023-03-15 14:56:35 +0100

    Loading screen loading

---
+++ New2DGame/Assets/Scripts/Text/ShowText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowText : MonoBehaviour
+{
+    public string textValue;
+    public TMP_Text textElement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
