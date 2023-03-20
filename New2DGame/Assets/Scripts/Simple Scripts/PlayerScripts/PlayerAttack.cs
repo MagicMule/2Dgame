a195d36f4e5ff1d63b2c4092bb07bcf80aea5207
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-03-20 10:42:03 +0100

    Revert "1-3"
    This reverts commit fcd737e427c692e92b615c2c1887f259eaceb061.

--- New2DGame/Assets/Scripts/Simple Scripts/PlayerScripts/PlayerAttack.cs
+++ New2DGame/Assets/Scripts/Simple Scripts/PlayerScripts/PlayerAttack.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     public GameObject sword;
-    public GameObject missile;
-
     public bool swordAttackReady = true;
 
     public float swordAttackDeley = 0.5f;
@@ -17,7 +15,6 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         UseSwordAttack();
-        ShotMissile();
     }
 
     //Attack with the gameobjekt "Sword"
@@ -29,14 +26,6 @@ public class PlayerAttack : MonoBehaviour
 
             sword.SetActive(true);
             StartCoroutine(SwordAttack());
-        }
-    }
-    // Instansate Missile, att missle prefab postion
-    void ShotMissile()
-    {
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            Instantiate(missile, transform.position + new Vector3(0.5f, 0, 0), transform.rotation);
         }
     }
     // The objekts becomes unactive withen 0.5 secunds.
