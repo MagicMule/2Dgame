40f9706cb09f84e877870f68ab65af9cc1a2c201
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-03-20 10:43:10 +0100

    Script files oragnstion

---
+++ New2DGame/Assets/Scripts/Simple Scripts/MoveObjektScripts/MoveForward.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    private Rigidbody2D rB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rB.velocity = new Vector2(1, 0);
+    }
+}
