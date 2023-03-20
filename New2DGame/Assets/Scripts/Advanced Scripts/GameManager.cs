88338d56cf26fb8644d6b88f0bd0c6b5c4c6b14e
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-03-20 13:57:29 +0100

    Removed scenes and scripts
    Alterd GameManger, Removed the intro menu with graphics

--- New2DGame/Assets/Scripts/Advanced Scripts/GameManager.cs
+++ New2DGame/Assets/Scripts/Advanced Scripts/GameManager.cs
@@ -10,10 +10,9 @@ public class GameManager : MonoBehaviour
     public int world { get; private set; }
     public int stage { get; private set; }
     public int lives { get; private set; }
-    public int cherry { get; private set; }
 
-    public GameObject gameOverUI; // UI element to be seen at gameover
-    public float resetDelay = 2f;
+    //public GameObject gameOverUI; // UI element to be seen at gameover
+ 
 
 
     private void Awake()
@@ -29,8 +28,6 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
     }
-
-    // (?)
     private void OnDestroy() 
     {
         if (Instance == this)
@@ -41,19 +38,13 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        Application.targetFrameRate = 60; // Set the max framerate to 60
-
-        NewGame();
-        FindObjectOfType<PlayerLife>().OnDeath += GameOver;
-        gameOverUI.SetActive(false);
 
     }
 
-    // A new game resets lives and score (cherry) and loads level 1 (World 1, stage 1) 
+    // A new game resets lives and loads level 1 (World 1, stage 1) 
     public void NewGame()
     {
         lives = 3;
-        cherry = 0;
 
         LoadLevel(1, 1);
     }
@@ -61,7 +52,7 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         //AudioManager.instance.PlaySound2D("GameOver");
-        gameOverUI.SetActive(true); // Activate the game Over screan
+        //gameOverUI.SetActive(true); // Activate the game Over screan
 
         NewGame(); // Cange to a new game
     }
@@ -78,46 +69,6 @@ public class GameManager : MonoBehaviour
     public void NextLevel()
     {
         LoadLevel(world, stage + 1); //Stay in curent world and load nex stage
-    }
-
-    public void ResetLevel(float resetdelay)
-    {
-        Invoke(nameof(ResetLevel), resetdelay);
-    }
-
-    //resets the curent scene
-    public void ResetLevel()
-    {
-        lives--; //lose one Life
-
-        // if lives != 0 load the curent world and satge apon reset
-        if (lives > 0)
-        {
-            LoadLevel(world, stage);
-        }
-        // if lives <= 0 exeute GameOver funktion
-        else
-        {
-            GameOver();
-        }
-    }
-
-    // Adds Cherry(score)
-    public void AddCherry()
-    {
-        cherry++; // increas the cherry amount by 1
-
-        if (cherry == 100) // if cherry amount = 100, the player gets an extra life, AddLife
-        {
-            cherry = 0;
-            AddLife();
-        }
-    }
-
-    // Increase life count by 1
-    public void AddLife()
-    {
-        lives++;
     }
 
     // UI Input
