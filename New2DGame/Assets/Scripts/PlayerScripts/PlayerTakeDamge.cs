a88d1c52869c0505ee572374b204660c13147b0d
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-03-23 18:28:41 +0100

    Coments added to GameManger script

--- New2DGame/Assets/Scripts/PlayerScripts/PlayerTakeDamge.cs
+++ New2DGame/Assets/Scripts/PlayerScripts/PlayerTakeDamge.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerTakeDamge : MonoBehaviour
 {
+    //
     public int playerPushBackMagnitude = 40;
     public GameManagerSS gameManagerScript;
 
@@ -12,16 +13,20 @@ public class PlayerTakeDamge : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Diration player is shoved wwhen hitt
         Vector2 awayFromEnemyPushBack = new Vector2(transform.position.x - collision.transform.position.x, transform.position.y - transform.position.y).normalized;
 
         if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Missile"))
         {
+            //Uppdat of UI when player hit
             gameManagerScript.UpdatePlayerHPUI();
+
+            //Push back player
             playerRB.AddForce(awayFromEnemyPushBack * playerPushBackMagnitude ,ForceMode2D.Impulse);
 
             Debug.Log(gameObject.name + " hit by " + collision.gameObject.name);
 
-            if (collision.gameObject.CompareTag("Missile")) // if hit by missile, destriy missile
+            if (collision.gameObject.CompareTag("Missile")) // if hit by missile, destroy missile
             {
                 Destroy(collision.gameObject);
             }
