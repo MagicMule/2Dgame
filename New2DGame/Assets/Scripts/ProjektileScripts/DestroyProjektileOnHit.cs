dcec31abc320edd69d00a315febd6bae5d0406b7
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-05-03 11:34:43 +0200

    Projketile hit sound, Player help text
    1-3

--- New2DGame/Assets/Scripts/ProjektileScripts/DestroyProjektileOnHit.cs
+++ New2DGame/Assets/Scripts/ProjektileScripts/DestroyProjektileOnHit.cs
@@ -8,11 +8,23 @@ public class DestroyProjektileOnHit : MonoBehaviour
      * If this objekt colides with a sword objket is is destroyed. Used on enemy projektiles.
      * Player "Diflekts" enemy projektiles
      */
+    public AudioSource PlayerAudio;
+    public AudioClip projektileDestroySound;
+    public float destroySoundVolume = 1.0f;
+
+    private GameObject playerGameObjekt;
+
+    private void Start()
+    {
+        playerGameObjekt = GameObject.FindGameObjectWithTag("Player");
+        PlayerAudio = playerGameObjekt.GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Sword"))
         {
             Debug.Log("ProjektileHit!");
+            PlayerAudio.PlayOneShot(projektileDestroySound, destroySoundVolume);
             Destroy(gameObject);
         }
     }
